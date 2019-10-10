98dd4afd2b0bdafbadcf5adcda4e83649dacb9b3
AlexBumble <a.buzov@borderway.ru>
2019-10-10 14:05:31 +0300

    Refactor

--- Haskell/Is this a triangle?/solution.hs
+++ Haskell/Is this a triangle?/solution.hs
@@ -3,5 +3,4 @@ module Codewars.Triangles where
     import Data.List (sort)
     
     isTriangle :: Int -> Int -> Int -> Bool
-    isTriangle a b c = if (a > 0 && b > 0 && c > 0) && 
-        (a + b > c && a + c > b && b + c > a) then True else False
+    isTriangle a b c = aa + b > c && a + c > b && b + c > a
