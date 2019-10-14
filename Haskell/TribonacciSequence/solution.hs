9cf697b48edac81cbd67261ac5c6f8aa24359a06
AlexBumble <a.buzov@borderway.ru>
2019-10-14 11:18:55 +0300

    update

--- Haskell/TribonacciSequence/solution.hs
+++ Haskell/TribonacciSequence/solution.hs
@@ -1,6 +1,15 @@
+-- My solution
+
 tribonacci :: Num a => (a, a, a) -> Int -> [a]
 tribonacci _ 0         = []
 tribonacci (a, b, c) n = take n ([a,b,c] ++ bonacci [a,b,c])
 
 bonacci :: Num a => [a] -> [a]
 bonacci [a,b,c] = [(a+b+c)] ++ bonacci [b,c,(a+b+c)]
+
+
+-- Best solution
+
+tribonacci :: Num a => (a, a, a) -> Int -> [a]
+tribonacci _ n | n < 1 = []
+tribonacci (a, b, c) n = a : tribonacci (b, c, a+b+c) (n-1)
